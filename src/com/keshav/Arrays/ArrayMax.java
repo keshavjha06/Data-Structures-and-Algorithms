25cf2715dfe5286f357b1e9613442f0ed220a867
keshav.jha <keshav.jha@medibuddy.in>
2022-01-22 23:20:27 +0530

    First commit

---
+++ src/com/keshav/Arrays/ArrayMax.java
@@ -0,0 +1,42 @@
+package com.keshav.Arrays;
+
+public class ArrayMax {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 23 , 9, 20 , 24};
+        System.out.println(max(arr));
+        System.out.println(maxRange(arr, 2, 4));
+
+    }
+
+    static int maxRange(int[] arr,int start,int end) {
+
+        if(end > start) {
+            return  -1;
+        }
+
+        if(arr == null) {
+            return  -1;
+        }
+        int maxVal = arr[start];
+        for (int i = start; i <= end; i++) {
+            if (arr[i] > maxVal) {
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+
+    static int max(int[] arr) {
+
+        if(arr.length == 0) {
+            return  -1;
+        }
+        int maxVal = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > maxVal) {
+                maxVal = arr[i] ;
+            }
+        }
+        return maxVal;
+    }
+}
---
+++ src/com/keshav/Arrays/Swap.java
@@ -0,0 +1,31 @@
+package com.keshav.Arrays;
+
+import java.util.Arrays;
+
+public class Swap {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 7, 9, 10, 56};
+     //  swap(arr, 1, 3);
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            //swap
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    static void swap(int[] arr, int num1, int num2) {
+        int temp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = temp;
+    }
+
+}
+
