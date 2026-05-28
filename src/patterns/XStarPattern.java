b2746c6074e9bf09ce2d54ef0365457df1555086
Keshav Jha <keshavjha0610@gmail.com>
2026-05-28 13:07:00 +0530

    refactor: simplify XStarPattern input to use a single dimension for square grid generation

--- src/patterns/XStarPattern.java
+++ src/patterns/XStarPattern.java
@@ -5,13 +5,11 @@ import java.util.Scanner;
 public class XStarPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter no of rows ");
-        int row = sc.nextInt();
-        System.out.println("Enter no of columns ");
-        int col = sc.nextInt();
-        for (int i = 1; i <= row; i++) {
-            for (int j = 1; j <= col; j++) {
-                if (i == j || j == row + 1 - i) {
+        System.out.println("Enter n: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j || j == n + 1 - i) {
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");
