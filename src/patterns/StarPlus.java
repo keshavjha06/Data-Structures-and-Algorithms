52eed9e12ea8f37c1dc9699c945ef9c5f4a400d5
Keshav Jha <keshavjha0610@gmail.com>
2026-05-28 13:14:37 +0530

    refactor: simplify StarPlus pattern to use a single dimension for square grid generation

--- src/patterns/StarPlus.java
+++ src/patterns/StarPlus.java
@@ -6,12 +6,10 @@ public class StarPlus {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter no of rows ");
-        int row = sc.nextInt();
-        System.out.println("Enter no of columns ");
-        int col = sc.nextInt();
-        for (int i = 1; i <= row; i++) {
-            for (int j = 1; j <= col; j++) {
-                if (i == (row / 2) + 1 || j == (col / 2) + 1) { // mid = (row/col / 2 ) + 1
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == (n / 2) + 1 || j == (n / 2) + 1) { // mid = (n / 2 ) + 1
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");
